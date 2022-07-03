0571160e817cfc998b72cbb0661191483d0b19a0
ats3md <ats3md@umsystem.edu>
2022-07-03 13:46:39 -0500

    added outer game loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Game connectFour = new Game();
-        connectFour.gameLoop();
+        boolean playing = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (playing) {
+            Game connectFour = new Game();
+            connectFour.gameLoop();
+
+            System.out.println("Press Q to quit. Press anything else to play again: ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("Q") || userInput.equals("q")) {
+                playing = false;
+            }
+
+        }
+
+        System.out.println("Exiting program.");
+
     }
 }
